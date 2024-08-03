fcbeb5dfd66f56318c02662e88ce443174880fa6
karsh <utkarsh@cyberfear.com>
2024-08-03 15:18:43 +1000

    transform actions
    + added implementations to all previously defined transform actions
    ~ renamed numericals to charindx

--- src/transform.c
+++ src/transform.c
@@ -5,3 +5,115 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+// ALL UPPER CASE
+void upper(char** aText) {
+	for (int i = 0; (*aText)[i] != '\0'; i++) {
+		(*aText)[i] = toupper((*aText)[i]);
+	}
+	printf("%s\n", *aText);
+}
+
+// all lower case
+void lower(char** aText) {
+	for (int i = 0; (*aText)[i] != '\0'; i++) {
+		(*aText)[i] = tolower((*aText)[i]);
+	}
+	printf("%s\n", *aText);
+}
+
+// Sentence case. Capital only after fullstop.
+void sentence(char** aText) {
+	bool lNewSentence = 1;
+
+	for (int i = 0; (*aText)[i] != '\0'; i++) {
+		if (lNewSentence && isalpha((*aText)[i])) {
+			(*aText)[i] = toupper((*aText)[i]);
+			lNewSentence = false;
+
+		} else if (isspace((*aText)[i])) {
+			if (i > 0 && ((*aText)[i-1] == '.' ||
+						(*aText)[i-1] == '!' ||
+						(*aText)[i-1] == '?'))
+			{
+				lNewSentence = true;
+			}
+
+		} else {
+			(*aText)[i] = tolower((*aText)[i]);
+		}
+	}
+
+	printf("%s\n", *aText);
+}
+
+// Every Word Capital
+void word(char** aText) {
+	bool lInWord = 0;
+
+	for (int i = 0; (*aText)[i] != '\0'; i++) {
+		if (isalpha((*aText)[i])) {
+			if (!lInWord) {
+				(*aText)[i] = toupper((*aText)[i]);
+				lInWord = true;
+			} else {
+				(*aText)[i] = tolower((*aText)[i]);
+			}
+		} else {
+			lInWord = false;
+		}
+	}
+
+	printf("%s\n", *aText);
+}
+
+// ebgngr nyy punenpgref ol guvegrra
+void rotate13(char** aText) {
+	for (int i = 0; (*aText)[i] != '\0'; i++) {
+		if (isalpha((*aText)[i])) {
+			char base = isupper((*aText)[i]) ? 'A' : 'a';
+			(*aText)[i] = base + (((*aText)[i] - base + 13) % 26);
+		}
+	}
+	printf("%s\n", *aText);
+}
+
+// mOcK tExT
+void troll(char** aText) {
+	bool lSmall = true;
+
+	for (int i = 0; (*aText)[i] != '\0'; i++) {
+		if (lSmall) {
+			(*aText)[i] = tolower((*aText)[i]);
+		} else {
+			(*aText)[i] = toupper((*aText)[i]);
+		}
+
+		lSmall = !lSmall;
+	}
+	printf("%s\n", *aText);
+}
+
+// cAPITAL BECOMES LOWER
+void flip(char** aText) {
+	for (int i = 0; (*aText)[i] != '\0'; i++) {
+		if (isupper(*aText[i])) {
+			(*aText)[i] = tolower((*aText)[i]);
+		} else {
+			(*aText)[i] = toupper((*aText)[i]);
+		}
+	}
+	printf("%s\n", *aText);
+}
+
+// 1618091420 0308011819 0119 2008050918 0112160801020520 0914040524
+void charindx(char** aText) {
+    for (int i = 0; (*aText)[i] != '\0'; i++) {
+        if (isalpha((*aText)[i])) {
+            printf("%02d", tolower((*aText)[i]) - 'a' + 1);
+        } else {
+            printf("%c", (*aText)[i]);
+        }
+    }
+    printf("\n");
+}
+
--- src/transform.h
+++ src/transform.h
@@ -11,5 +11,5 @@ void word(char** aText);
 void rotate13(char** aText);
 void flip(char** aText);
 void troll(char** aText);
-void numericals(char** aText);
 
+void charindx(char** aText);
