492c07024f3b9dfdab16551d54d73221a98e19b5
karsh <utkarsh@cyberfear.com>
2024-08-07 14:21:53 +1000

    fix segmentation fault
    ~ fixed code with storing data in Data type var
    ~ let lsp change code standard, so long Markus's ( spaces in brackets )

--- src/commands.c
+++ src/commands.c
@@ -10,47 +10,46 @@
 #include "commands.h"
 #include "printbreak.h"
 
-
 //- Local Helper ---------------------------------------------------------------
 
 // check if input is same as any existing commands
 bool check_cmd(char** aInput, const char* aShort, const char* aLong) {
-	return !( strcmp(*aInput, aShort) && strcmp(*aInput, aLong) );
+	return !(strcmp(*aInput, aShort) && strcmp(*aInput, aLong));
 }
 
 // get data from pipes
 char* get_piped_data() {
-	char *inputBuffer = NULL;
-	size_t bufferSize = 0;
-	size_t inputSize = 0;
-	int ch;
+	char *Result = NULL; // input buffer
+	size_t lBufferSize = 0;
+	size_t lInputSize = 0;
+	int lSingleChar;
 
-	while ((ch = getchar()) != EOF) {
-		if (inputSize >= bufferSize) {
-			bufferSize += 256;
-			char* newBuffer = realloc(inputBuffer, bufferSize);
+	while ((lSingleChar = getchar()) != EOF) {
+		if (lInputSize >= lBufferSize) {
+			lBufferSize += 256;
+			char* newBuffer = realloc(Result, lBufferSize);
 			error_mem_alloc(&newBuffer);
-			inputBuffer = newBuffer;
+			Result = newBuffer;
 		}
-		inputBuffer[inputSize++] = (char)ch;
+		Result[lInputSize++] = (char)lSingleChar;
 	}
 
-	if (inputSize >= bufferSize) {
-		bufferSize += 1;
-		char* newBuffer = realloc(inputBuffer, bufferSize);
+	if (lInputSize >= lBufferSize) {
+		lBufferSize += 1;
+		char* newBuffer = realloc(Result, lBufferSize);
 		error_mem_alloc(&newBuffer);
-		inputBuffer = newBuffer;
+		Result = newBuffer;
 	}
-	inputBuffer[inputSize] = '\0';
-	return inputBuffer;
-}
+	Result[lInputSize] = '\0';
 
+	return Result;
+}
 
 //- Export Functions -----------------------------------------------------------
 
 // if user requested help, print help menu and exit
 void check_help(char** aInput) {
-	if ( check_cmd(aInput, "-h", "--help") ) {
+	if (check_cmd(aInput, "-h", "--help")) {
 		print_help();
 	}
 }
@@ -58,12 +57,13 @@ void check_help(char** aInput) {
 // return Action if triggered command was valid, else terminate
 uint get_action_id(char** aInput) {
 	for (uint i = 0; i < ActionsCount; i++) {
-		if ( check_cmd(aInput, Actions[i].STrigger, Actions[i].LTrigger) ) {
+		if (check_cmd(aInput, Actions[i].STrigger, Actions[i].LTrigger)) {
 			return i;
 		}
 	}
 
-	return ActionsCount;
+	error_invalid_command((const char**)aInput);
+	return ActionsCount; // this will never be reached due to the above exit call
 }
 
 // check for data in args and pipe and return it
@@ -71,15 +71,15 @@ Data get_data(const int* argc, char*** argv, const Call* aType) {
 	Data Result;
 	uint lArgsCount = 3;
 
-	if ( *aType == Transform ) {
+	if (*aType == Transform) {
 		lArgsCount = 2;
 	}
 
-	if ( *argc == lArgsCount ) {
+	if (*argc == lArgsCount) {
 		Result.Value = get_piped_data();
 		Result.InHeap = true;
 	} else {
-		Result.Value = *argv[lArgsCount];
+		Result.Value = (*argv)[lArgsCount];
 		Result.InHeap = false;
 	}
 
@@ -88,8 +88,7 @@ Data get_data(const int* argc, char*** argv, const Call* aType) {
 
 // if data is stored in heap, free it
 void free_data(Data* lData) {
-	if ( lData->InHeap ) {
+	if (lData->InHeap) {
 		free(lData->Value);
 	}
 }
-
